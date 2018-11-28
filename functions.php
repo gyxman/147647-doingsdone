40a0866def11b3d3a8f1da4c7f9b91425ab7862a
Roman Aleksyutin <gyxman@yandex.ru>
2018-11-28 09:59:36 +0300

    add hw 3-1

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
---
+++ templates/index.php
@@ -0,0 +1,46 @@
+<h2 class="content__main-heading">Список задач</h2>
+
+<form class="search-form" action="index.php" method="post">
+    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+
+    <input class="search-form__submit" type="submit" name="" value="Искать">
+</form>
+
+<div class="tasks-controls">
+    <nav class="tasks-switch">
+        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+        <a href="/" class="tasks-switch__item">Повестка дня</a>
+        <a href="/" class="tasks-switch__item">Завтра</a>
+        <a href="/" class="tasks-switch__item">Просроченные</a>
+    </nav>
+
+    <label class="checkbox">
+        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?=$show_complete_tasks ? 'checked' : ''?>>
+        <span class="checkbox__text">Показывать выполненные</span>
+    </label>
+</div>
+
+<table class="tasks">
+    <?php foreach ($tasks as $task => $task_val): ?>
+        <?php if ($show_complete_tasks || $task_val['complete'] === 'Нет') : ?>
+            <tr class="tasks__item task <?php if ($task_val['complete'] === 'Да'): ?>task--completed<?php endif; ?>">
+                <td class="task__select">
+                    <label class="checkbox task__checkbox">
+                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                        <span class="checkbox__text"><?=$task_val['name'];?></span>
+                    </label>
+                </td>
+
+                <!-- оставил на будущее
+                <td class="task__file">
+                    <a class="download-link" href="#">Home.psd</a>
+                </td>
+                -->
+
+                <td class="task__date">
+                    <?=$task_val['date'];?>
+                </td>
+            </tr>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</table>
