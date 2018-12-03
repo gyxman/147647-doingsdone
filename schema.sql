5bb9573d3f306191351353b022f019fd6f1d0db3
Roman Aleksyutin <gyxman@yandex.ru>
2018-12-03 09:37:41 +0300

    resolve pr comments

--- schema.sql
+++ schema.sql
@@ -3,28 +3,32 @@ DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
 CREATE TABLE projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR NOT NULL UNIQUE,
-    user_id INT
-);
+    id int(11) NOT NULL AUTO_INCREMENT,
+    name char(1) NOT NULL,
+    user_id int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY name (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_completed TIMESTAMP,
-    is_completed TINYINT DEFAULT 0,
-    name CHAR NOT NULL,
-    file_link CHAR,
-    date_target TIMESTAMP,
-    user_id INT NOT NULL,
-    cat_id INT NOT NULL
-);
-CREATE INDEX name ON tasks(name);
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date_create timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+   date_completed timestamp NULL DEFAULT NULL,
+   is_completed tinyint(4) DEFAULT '0',
+   name char(1) NOT NULL,
+   file_link char(1) DEFAULT NULL,
+   date_target timestamp NULL DEFAULT NULL,
+   user_id int(11) NOT NULL,
+   cat_id int(11) NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email CHAR(128) NOT NULL UNIQUE,
-    name CHAR,
-    password CHAR(64)
-);
+   id int(11) NOT NULL AUTO_INCREMENT,
+   date_add timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+   email char(128) NOT NULL,
+   name char(1) DEFAULT NULL,
+   password char(64) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY email (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
