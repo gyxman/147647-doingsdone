4f5d4dd6ca57d40ce5645888b94c25bc9ef2447d
Roman Aleksyutin <gyxman@yandex.ru>
2018-12-10 09:26:59 +0300

    add hw 5-1

--- index.php
+++ index.php
@@ -10,19 +10,15 @@ else {
     $result_user = mysqli_query($link, $sql_user);
     $user = mysqli_fetch_assoc($result_user);
 
-    $sql_projects = 'SELECT DISTINCT a.`name` FROM projects a LEFT JOIN tasks b ON a.id = b.project_id';
+    $sql_projects = 'SELECT DISTINCT a.`name`, COUNT(a.`name`) as count FROM projects a LEFT JOIN tasks b ON a.id = b.project_id GROUP BY a.`name`';
     $result_projects = mysqli_query($link, $sql_projects);
 
     $sql_tasks = 'SELECT * FROM tasks';
     $result_tasks = mysqli_query($link, $sql_tasks);
 
-    $sql_tasks_count = 'SELECT `project_id`, SUM(project_id) FROM tasks GROUP BY `project_id`';
-    $result_tasks_count = mysqli_query($link, $sql_tasks_count);
-
     if ($result_projects && $result_tasks) {
         $projects = mysqli_fetch_all($result_projects, MYSQLI_ASSOC);
         $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
-        $tasks_count = mysqli_fetch_all($result_tasks_count, MYSQLI_ASSOC);
         $page_content = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
         $sidebar = include_template('sidebar.php', ['title' => 'Проекты', 'projects' => $projects]);
         $layout_content = include_template('layout.php', [
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -3,7 +3,7 @@
         <?php foreach ($projects as $project): ?>
             <li class="main-navigation__list-item">
                 <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($project[name]); ?></a>
-                <span class="main-navigation__list-item-count">123</span>
+                <span class="main-navigation__list-item-count"><?=$project[count]; ?></span>
             </li>
         <?php endforeach; ?>
     </ul>
